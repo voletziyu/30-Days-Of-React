2e7cfdb0ca0e685ebc86c78051eca0ae36b2ded5
voletziyu <voletziyu@gmail.com>
2021-08-24 18:18:35 +0800

    从表单中获取多个输入框数据

--- solutions/day-12/src/index.js
+++ solutions/day-12/src/index.js
@@ -7,26 +7,78 @@ class App extends Component {
   // 初始化状态
   state = {
     firstName: "",
+    lastName: '',
+    country: '',
+    title: '',
   };
   handleChange = (e) => {
-    const value = e.target.value
-    this.setState({ firstName: value })
+    // 我们像这样去e.target.name, e.target.value 得到 name和value
+    // 但是也可以解构去得到这两个值 
+    const { name, value } = e.target
+    // [variablename]在一个对象中使用一个变量名称作为键。
+    // name指的是输入元素的名称属性
+    this.setState({ [name]: value })
+  }
+  handleSubmit = (e) => {
+    // e.preventDefault() 停止表单元素的默认行为, 特别刷新页面
+    e.preventDefault()
+    // 这个地方是我们连接后端api 去发送数据到数据库的地方
+    console.log(this.state)
   }
   render() {
-    // 访问state值 这个值将插入进input中的value属性中
-    const firstName = this.state.firstName
+    // 解构获得state的值
+    const { firstName, lastName, title, country } = this.state
     return (
       <div className="App">
-        <label htmlFor="firstName">first Name:</label>
-        <input
-          type='text'
-          id='firstName'
-          name='first Name'
-          placeholder='First Name'
-          value={firstName}
-          onChange={this.handleChange}
-        />
-        <h1>{this.state.firstName}</h1>
+        <h3>Add Student</h3>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor='firstName'>First Name: </label>
+            <input
+              type='text'
+              id='firstName'
+              name='firstName'
+              placeholder='First Name'
+              value={firstName}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='lastName'>Last Name: </label>
+            <input
+              type='text'
+              id='lastName'
+              name='lastName'
+              placeholder='Last Name'
+              value={lastName}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='country'>Country: </label>
+            <input
+              type='text'
+              id='country'
+              name='country'
+              placeholder='Country'
+              value={country}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='country'>Title: </label>
+            <input
+              type='text'
+              id='title'
+              name='title'
+              placeholder='Title'
+              value={title}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <button className='btn btn-success'>Submit</button>
+        </form>
       </div>
     );
   }
